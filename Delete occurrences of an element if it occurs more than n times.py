87b335db2d39bc5784d0e2f36db76c8c0d71271b
sinchanapuneeth <sinchanapuneeth@gmail.com>
2026-08-19 00:08:24 +0530

    Update Delete occurrences of an element if it occurs more than n times.py
    final_solution, included with the best rated solution,

--- Delete occurrences of an element if it occurs more than n times.py
+++ Delete occurrences of an element if it occurs more than n times.py
@@ -1,32 +1,27 @@
-x = "123444"
-y = list(x)
-n = 1
+def delete_nth(order,max_e):
+    final_list = []
+    dict_final_list = {}
+    
+    for a in order:
+        if a not in final_list:
+            dict_final_list[a] = 0
+        else:
+            continue
 
-dictionary = {}
-for c in y:
-    if c in dictionary:
-        dictionary[c] += 1
-    else:
-        dictionary[c] = 1
+    for a in order:
+        if dict_final_list[a] < max_e:
+            final_list.append(a)
+            dict_final_list[a] += 1
+            
+    return(final_list)
 
-print(dictionary)
-d = []
-d_dict = {}
+#def delete_nth(order,max_e):
+#    ans = []
+#    for o in order:
+#        if ans.count(o) < max_e: ans.append(o)
+#    return ans
 
-for c in y:
-    if c not in d:
-        d_dict[c] = 0
-
-for c in y:
-    if d_dict[c] < n:
-        d.append(c)
-        d_dict[c] += 1
-    else:
-        continue
-
-
-
-print(d_dict['4'])
-print(d_dict)
-print(d)
-
+#differences between efficient code and mine:
+#combined my two loops in one
+#did not use many nested ifs
+#BUT, my code runs in O(n), while the second one runs in O(n ^2)!
