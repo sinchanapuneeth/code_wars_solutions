90b54efe14c53fcb2385dc308483ad6b4a0267e5
sinchanapuneeth <sinchanapuneeth@gmail.com>
2026-08-19 19:01:23 +0530

    Add files via upload

---
+++ Moving Zeros To The End.py
@@ -0,0 +1,18 @@
+x =[1, 0, 1, 2, 0, 1, 3]
+y = []
+zero_num = 0
+for c in x:
+    if c != 0:
+        y.append(c)
+    else:
+        zero_num += 1
+
+print(y, zero_num)
+
+current_num = 0
+
+while current_num <= zero_num:
+    y.append(0)
+    current_num += 1
+
+print(y, current_num)
